66ec8631d646174c51cc020eab7bbf08e91adcf7
DESKTOP-A0EDSUE\kauan <kauanmellocancelier2005@gmail.com>
2023-09-15 15:39:44 -0300

    Implementado interface de repositório de itens de venda.

---
+++ src/main/java/br/com/mercadocancelier/repository/ItemVendaRepository.java
@@ -0,0 +1,19 @@
+package br.com.mercadocancelier.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.mercadocancelier.entity.ItemVenda;
+import br.com.mercadocancelier.entity.Venda;
+
+public interface ItemVendaRepository extends JpaRepository<ItemVenda, Integer>{
+	
+	@Query(value = "SELECT iv FROM ItemVenda iv WHERE iv.id = :id")
+	public ItemVenda buscarPor(@Param("id") Integer id);
+
+	 @Query(value = "SELECT iv FROM ItemVenda iv WHERE iv.venda = :venda")
+	 public List<ItemVenda> listarPor(@Param("venda")Venda venda);
+}
